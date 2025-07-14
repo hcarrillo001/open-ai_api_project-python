3d3a6436aa5d751713a840278a76abd6b2167725
hcarrillo001 <51094689+hcarrillo001@users.noreply.github.com>
2025-07-13 18:29:15 -0600

    Add files via upload
    First commit

---
+++ OpenAiPractice/OpenAiController.py
@@ -0,0 +1,18 @@
+from importlib.util import source_hash
+
+import openai
+import tkinter as tk
+
+from TkInterWindowGui import TkinterWindowGui
+#key will be placed in a file names sk.py
+from sk import my_sk
+
+
+
+
+def main():
+    tkinter_window = TkinterWindowGui()
+
+
+if __name__ == "__main__":
+    main()
---
+++ OpenAiPractice/README.md
@@ -0,0 +1,14 @@
+
+# OpenAiPractice.py
+
+## Authors 
+    Hanns Carrillo 
+
+Tasks 
+
+    • Create Open AI account
+    • Add funds to account
+    • Create token 
+
+    
+
